88b267747d551bceeb46575ae5f284ae138a851e
Atik Yumbay <70615264+amaruyum@users.noreply.github.com>
2024-09-02 14:35:51 -0500

    Update weather.service.spec.ts

--- src/app/weather.service.spec.ts
+++ src/app/weather.service.spec.ts
@@ -22,10 +22,9 @@ describe('WeatherService', () => {
   it('should fetch default weather data on creation', () => {
     const dummyWeatherData = { weather: [{ description: 'clear sky' }], main: { temp: 20 } };
 
-    // Intercepta la solicitud HTTP que se realiza cuando se carga el servicio
     const req = httpMock.expectOne(`http://api.openweathermap.org/data/2.5/weather?q=Quito&appid=584a471ebac0caa029abc42f2c8e4623&units=metric`);
     expect(req.request.method).toBe('GET');
-    req.flush(dummyWeatherData); // Simula la respuesta de la API
+    req.flush(dummyWeatherData); // Simulate request API
 
     service.weatherData$.subscribe(data => {
       expect(data).toEqual(dummyWeatherData);
